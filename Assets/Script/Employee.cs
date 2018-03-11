055de42aca69ed1d21cc99ae35c51cd74b6b70ce
Uttakran Renuman <uttakran.renuman@gmail.com>
2018-03-11 12:12:07 +0700

    unity init with some work

---
+++ Assets/Script/Employee.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Employee : MonoBehaviour {
+	public GameObject reset;
+	public GameObject manager_slider;
+	public GameObject programmer_slider;
+	public GameObject designer_slider;
+	public GameObject document_slider;
+
+	public float manager_value = 0;
+	public float programmer_value = 0;
+	public float designer_value = 0;
+	public float document_value = 0;
+
+	// Use this for initialization
+	void Start () {
+		Button reset_btn = reset.GetComponent<Button> ();
+		Random rand = new Random ();
+
+		reset_employee_status ();
+
+		//reset_btn.onClick.AddListener (set_zero_employee_status);
+		reset_btn.onClick.AddListener (reset_employee_status);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void reset_employee_status(){
+		manager_value = Random.Range (0.0f, 1.0f);
+		programmer_value = Random.Range (0.0f, 1.0f);
+		designer_value = Random.Range (0.0f, 1.0f);
+		document_value = Random.Range (0.0f, 1.0f);
+		manager_slider.GetComponent<Slider> ().value = manager_value;
+		programmer_slider.GetComponent<Slider> ().value = programmer_value;
+		designer_slider.GetComponent<Slider> ().value = designer_value;
+		document_slider.GetComponent<Slider> ().value = document_value;
+
+	}
+
+}
---
+++ Assets/Script/EmployeeSelectButton.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class EmployeeSelectButton : MonoBehaviour {
+
+	public GameObject selectButton;
+
+	public GameObject[] manager_status;
+	public GameObject[] programmer_status;
+	public GameObject[] designer_status;
+	public GameObject[] document_status;
+
+
+	// Use this for initialization
+	void Start () {
+		Button emploemployeeSelectButton = selectButton.GetComponent<Button> ();
+
+		emploemployeeSelectButton.onClick.AddListener (save_company_status);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void save_company_status() {
+		float manager_value = 0;
+		float programmer_value = 0;
+		float designer_value = 0;
+		float document_value = 0;
+		foreach (var item in manager_status) {
+			manager_value += item.GetComponent<Slider> ().value;
+		}
+		foreach (var item in programmer_status) {
+			programmer_value += item.GetComponent<Slider> ().value;
+		}
+		foreach (var item in designer_status) {
+			designer_value += item.GetComponent<Slider> ().value;
+		}
+		foreach (var item in document_status) {
+			document_value += item.GetComponent<Slider> ().value;
+		}
+		PlayerPrefs.SetFloat ("global_manager_status", manager_value);
+		PlayerPrefs.SetFloat ("global_programmer_status", programmer_value);
+		PlayerPrefs.SetFloat ("global_designer_status", designer_value);
+		PlayerPrefs.SetFloat ("global_document_status",document_value);
+
+		Debug.Log (PlayerPrefs.GetFloat ("global_manager_status", 0));
+	}
+
+}
